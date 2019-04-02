17d304b196248a801dd6085191c89f02fd89e1a4
andri <setiawanandri35@gmail.com>
2019-04-02 19:09:26 +0700

    update model

--- application/models/M_product.php
+++ application/models/M_product.php
@@ -28,7 +28,7 @@ class M_product extends CI_Model
             $this->db->like('updated_at', $params['updated_at']);
 
         $this->db->select('*');
-        $this->db->from('product');
+        $this->db->from('satuan');
         $query = $this->db->get();
 
         if(!empty($params['id']))
@@ -43,7 +43,7 @@ class M_product extends CI_Model
 
         // Select record
         $this->db->select('id, nama');
-        $query = $this->db->get('product')->result();
+        $query = $this->db->get('satuan')->result();
 
         foreach($query as $q) {
             $buffer[$q->id] = $q->nama;
